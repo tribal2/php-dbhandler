6c9735234e4427bb6ecdecc2a2b1f0b43dc14e26
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-11-15 20:51:49 +0000

    feat(DbTransaction): add new class to manage transactions

---
+++ src/DbTransaction.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tribal2;
+
+use Exception;
+
+class DbTransaction {
+
+
+  public static bool $throw = FALSE;
+
+
+  public static function begin(): bool {
+    $dbh = PDOSingleton::get();
+
+    if ($dbh->inTransaction()) {
+      return self::errorHandler('Ya existe una transacción activa.');
+    }
+
+    return $dbh->beginTransaction();
+  }
+
+
+  public static function commit(): bool {
+    $dbh = PDOSingleton::get();
+
+    if (!$dbh->inTransaction()) {
+      return self::errorHandler('No hay ninguna transacción activa.');
+    }
+
+    if (PDOSingleton::getCommitsMode() === PDOCommitModeEnum::OFF) {
+      return self::errorHandler('Los commits están desabilitados.');
+    }
+
+    return $dbh->commit();
+  }
+
+
+  public static function rollback(): bool {
+    $dbh = PDOSingleton::get();
+
+    if (!$dbh->inTransaction()) {
+      return self::errorHandler('No hay ninguna transacción activa.');
+    }
+
+    return $dbh->rollBack();
+  }
+
+
+  public static function check() {
+    $dbh = PDOSingleton::get();
+
+    return $dbh->inTransaction();
+  }
+
+
+  private static function errorHandler($msg): bool {
+    try {
+      if (self::$throw) {
+        throw new Exception($msg);
+      }
+
+      return FALSE;
+    }
+
+    catch (Exception $e) {
+      throw $e;
+    }
+  }
+
+
+}
