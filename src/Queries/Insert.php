6e27f3065b59c1e1bc7672c0e6374cd990bfbd7e
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-11-27 21:05:51 +0000

    feat(Insert): add new class to execute INSERT queries

---
+++ src/Queries/Insert.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Tribal2\DbHandler\Queries;
+
+use Exception;
+use PDO;
+
+use Tribal2\DbHandler\PDOBindBuilder;
+use Tribal2\DbHandler\PDOSingleton;
+use Tribal2\DbHandler\Queries\Common;
+use Tribal2\DbHandler\Queries\Where;
+use Tribal2\DbHandler\Table\Columns;
+
+class Insert {
+
+  private string $table;
+  private Columns $dbColumns;
+  private array $values = [];
+
+
+  public static function into(string $table): self {
+    return new self($table);
+  }
+
+
+  private function __construct(string $table) {
+    $this->table = Common::quoteWrap($table);
+    $this->dbColumns = Columns::for($table);
+  }
+
+
+  public function value(string $column, $value): self {
+    if ($this->dbColumns->has($column)) {
+      Common::checkValue($value, $column);
+      $this->values[$column] = $value;
+    }
+
+    return $this;
+  }
+
+
+  public function values(array $values): self {
+    foreach ($values as $col => $value) {
+      $this->value($col, $value);
+    }
+
+    return $this;
+  }
+
+
+  public function execute(
+    ?PDO $pdo = NULL,
+    ?PDOBindBuilder $bindBuilder = NULL,
+  ): bool {
+    $_pdo = $pdo ?? PDOSingleton::get();
+
+    // Check if there are collisions
+    $this->checkForCollisions();
+
+    $query = $this->getSql();
+    $pdoStatement = $_pdo->prepare($query);
+
+    // Bind values
+    $bindBuilder = $bindBuilder ?? new PDOBindBuilder();
+    $bindBuilder->bindToStatement($pdoStatement);
+
+    // Execute query
+    return $pdoStatement->execute();
+  }
+
+
+  public function getValues(): array {
+    return $this->values;
+  }
+
+
+  public function getSql(?PDOBindBuilder $bindBuilder = NULL): string {
+    if (count($this->values) === 0) {
+      throw new Exception(
+        'You must provide at least one value to insert',
+        400,
+      );
+    }
+
+    $bindBuilder = $bindBuilder ?? new PDOBindBuilder();
+
+    $queryColumns = [];
+    $queryParams = [];
+
+    foreach ($this->values as $col => $value) {
+      $queryColumns[] = Common::quoteWrap($col);
+      $queryParams[] = $bindBuilder->addValueWithPrefix(
+        $value,
+        $col,
+        Common::checkValue($value, $col),
+      );
+    }
+
+    $qColumns = implode(', ', $queryColumns);
+    $qParams = implode(', ', $queryParams);
+
+    $query = "INSERT INTO {$this->table} ({$qColumns}) VALUES ({$qParams});";
+
+    return $query;
+  }
+
+
+  private function checkForCollisions() {
+    $isKeyAutoincrement = count($this->dbColumns->autoincrement) > 0;
+    if ($isKeyAutoincrement) return;
+
+    // If the primary key is not autoincremented we make sure it is not repeated
+    $wheres = [];
+    foreach ($this->dbColumns->key as $keyColName) {
+      if (isset($this->values[$keyColName])) {
+        $wheres[] = Where::equals(
+          $keyColName,
+          $this->values[$keyColName],
+        );
+      }
+    }
+
+    if (count($wheres) === 0) return;
+
+    $where = Where::and(...$wheres);
+    $exists = Select::from($this->table)
+      ->where($where)
+      ->fetchFirst();
+
+    if (!is_null($exists)) {
+      throw new Exception(
+        "The values you are trying to insert already exist in the database",
+        409,
+      );
+    }
+  }
+
+
+}
---
+++ tests/Unit/InsertTest.php
@@ -0,0 +1,111 @@
+<?php
+
+use Mockery\Mock;
+use Tribal2\DbHandler\PDOBindBuilder;
+use Tribal2\DbHandler\Queries\Insert;
+use Tribal2\DbHandler\Table\Columns;
+
+require_once __DIR__ . '/../Feature/_DbTestSchema.php';
+
+beforeAll(function () {
+  DbTestSchema::up();
+});
+
+afterAll(function () {
+  DbTestSchema::down();
+});
+
+describe('Builder', function () {
+
+  test('static factory', function () {
+    expect(Insert::into('my_table'))->toBeInstanceOf(Insert::class);
+  });
+
+});
+
+
+describe('Insert values', function () {
+
+  test('value() adds a column/value to the insert query', function () {
+    $values = Insert::into('test_table')
+      ->value('value', 'value1')
+      ->getValues();
+
+    expect($values)->toBeArray();
+    expect($values)->toHaveCount(1);
+    expect($values['value'])->toBe('value1');
+  });
+
+  test('values() adds multiple values correctly', function () {
+    $values = Insert::into('test_table')
+      ->values([
+        'key' => 'key1',
+        'value' => 'value',
+      ])
+      ->getValues();
+
+    expect($values)->toBeArray();
+    expect($values)->toHaveCount(2);
+    expect($values['key'])->toBe('key1');
+    expect($values['value'])->toBe('value');
+  });
+
+  test('value() and values() chained', function () {
+    $values = Insert::into('test_table')
+      ->value('key', 'key1')
+      ->values([
+        'value' => 25,
+        'created_at' => '2020-01-01 00:00:00',
+      ])
+      ->getValues();
+
+    expect($values)->toBeArray();
+    expect($values)->toHaveCount(3);
+    expect($values['key'])->toBe('key1');
+    expect($values['value'])->toBe(25);
+    expect($values['created_at'])->toBe('2020-01-01 00:00:00');
+  });
+
+  test('value() ignores columns that are not on the table', function () {
+    $values = Insert::into('test_table')
+      ->value('column1', 'value1')
+      ->getValues();
+
+    expect($values)->toBeArray();
+    expect($values)->toHaveCount(0);
+  });
+
+  test('value() throws on invalid value', function () {
+    Insert::into('test_table')
+      ->value('value', [ 1, 2, 3 ])
+      ->getValues();
+  })->throws(Exception::class);
+
+});
+
+
+describe('SQL', function () {
+
+  test('getSql() returns a valid SQL string', function () {
+    $bindBuilder = new PDOBindBuilder();
+    $sql = Insert::into('test_table')
+      ->value('key', 25)
+      ->values([
+        'value' => NULL,
+        'created_at' => '2020-01-01 00:00:00',
+      ])
+      ->getSql($bindBuilder);
+
+    $expected = "INSERT INTO `test_table` (`key`, `value`, `created_at`) "
+      . "VALUES (:key___1, :value___1, :created_at___1);";
+
+    expect($sql)->toBeString();
+    expect($sql)->toBe($expected);
+
+    $expected2 = "INSERT INTO `test_table` (`key`, `value`, `created_at`) "
+      . "VALUES (25, NULL, '2020-01-01 00:00:00');";
+
+    expect($bindBuilder->debugQuery($sql))->toBe($expected2);
+  });
+
+});
