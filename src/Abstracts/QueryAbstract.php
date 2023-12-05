ecb58ba87479ac9ff45115af734f7aa4bb86a419
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2023-12-05 18:16:04 +0000

    feat(QueryAbstract): add abstract method getSql and _execute method implementation

--- src/Abstracts/QueryAbstract.php
+++ src/Abstracts/QueryAbstract.php
@@ -4,7 +4,10 @@ namespace Tribal2\DbHandler\Abstracts;
 
 use Exception;
 use PDO;
+use PDOStatement;
 use Tribal2\DbHandler\Interfaces\CommonInterface;
+use Tribal2\DbHandler\Interfaces\PDOBindBuilderInterface;
+use Tribal2\DbHandler\PDOBindBuilder;
 use Tribal2\DbHandler\PDOSingleton;
 use Tribal2\DbHandler\Queries\Common;
 
@@ -16,6 +19,19 @@ abstract class QueryAbstract {
   // Dependencies
   protected PDO $_pdo;
   protected CommonInterface $_common;
+
+
+  // Abstract methods
+  /**
+   * Get the SQL query string
+   *
+   * @param PDOBindBuilderInterface|null $bindBuilder
+   *
+   * @return string
+   */
+  abstract public function getSql(
+    ?PDOBindBuilderInterface $bindBuilder = NULL,
+  ): string;
 
 
   public function __construct(
@@ -31,6 +47,32 @@ abstract class QueryAbstract {
     if (!isset($this->table)) {
       throw new Exception('Table name is not set');
     }
+  }
+
+
+  protected function _execute(
+    ?PDOBindBuilderInterface $bindBuilder = NULL,
+    ?PDO $pdo = NULL,
+  ): PDOStatement {
+    $_pdo = $pdo ?? $this->_pdo;
+
+    // Verify that the table name is set
+    $this->beforeExecute();
+
+    // Bind values
+    $bindBuilder = $bindBuilder ?? new PDOBindBuilder();
+
+    // Generate query
+    $query = $this->getSql($bindBuilder);
+
+    // Prepare query and bind values
+    $pdoStatement = $_pdo->prepare($query);
+    $bindBuilder->bindToStatement($pdoStatement);
+
+    // Execute query
+    $pdoStatement->execute();
+
+    return $pdoStatement;
   }
 
 
